744d5c6fe6f1aa5e44ca3d4e0b4a3ec31c8680c4
kimseunghoon <yoyo0421@naver.com>
2022-08-13 20:48:05 +0900

    멀티컬럼 리스트 레이아웃 완료

---
+++ MulticolumList/MultiColumList/MultiColumList/FrameworklCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FrameworklCollectionViewCell.swift
+//  MultiColumList
+//
+//  Created by 김승훈 on 2022/08/13.
+//
+
+import UIKit
+
+class FrameworklCollectionViewCell: UICollectionViewCell {
+    
+}
