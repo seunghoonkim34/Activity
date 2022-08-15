de91f47c9e883d1b598592a7942fdcba51ba1729
kimseunghoon <yoyo0421@naver.com>
2022-08-15 20:14:30 +0900

    레이아웃 연동 오류

---
+++ TabBarListView/TabBarListView/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SearchViewControllerCell.swift
+//  TabBarListView
+//
+//  Created by 김승훈 on 2022/08/15.
+//
+
+import UIKit
+
+class SearchViewControllerCell: UICollectionViewCell {
+    
+}
