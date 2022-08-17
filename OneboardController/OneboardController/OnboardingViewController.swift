a9035ce5bc2c1b27e3c29cc3f6615f878b8150d1
kimseunghoon <yoyo0421@naver.com>
2022-08-17 21:30:14 +0900

    레이아웃 생성

---
+++ OneboardController/OneboardController/OnboardingViewController.swift
@@ -0,0 +1,19 @@
+ //
+//  ViewController.swift
+//  OneboardController
+//
+//  Created by 김승훈 on 2022/08/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
