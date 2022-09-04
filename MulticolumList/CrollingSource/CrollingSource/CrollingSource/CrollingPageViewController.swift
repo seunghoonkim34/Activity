c347bcc37211e73755635568f96cb6d0f5177e49
kimseunghoon <yoyo0421@naver.com>
2022-09-04 21:05:39 +0900

    started Nanoproject

---
+++ MulticolumList/CrollingSource/CrollingSource/CrollingSource/CrollingPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CrollingPageViewController.swift
+//  CrollingSource
+//
+//  Created by 김승훈 on 2022/08/27.
+//
+
+import UIKit
+
+class CrollingPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
